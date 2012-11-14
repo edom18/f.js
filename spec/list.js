52b37eb562144f346c18233518cb7305d3f07fd9
edom18 <edo.m18@gmail.com>
2012-11-14 15:23:39 +0900

    added a test case.

---
+++ spec/list.js
@@ -0,0 +1,11 @@
+document.write('\
+<script type="text/template" id="jasmineBaseList">\
+f.mvc/\n\
+</script>\
+<script type="text/template" id="jasmineClassList">\
+../../js/f.mvc.js\n\
+</script>\
+<script type="text/template" id="jasmineTestList">\
+../_src/f.mvc/test.js\n\
+</script>\
+');
